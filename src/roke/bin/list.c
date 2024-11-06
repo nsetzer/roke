7c9d9ca7413b9ad2f0f7f9a2c2e630f062a36b77
Nick Setzer <nicksetzer@gmail.com>
2024-11-06 07:10:57 -0500

    cleanup the output of roke list: sort entries

--- src/roke/bin/list.c
+++ src/roke/bin/list.c
@@ -1,4 +1,6 @@
 
+#include <string.h>
+
 #include "roke/libroke_internal.h"
 
 argparse_spec_t spec[] = {
@@ -9,8 +11,37 @@ argparse_spec_t spec[] = {
 
     {0, 0, 0, "Other:"},
     {0, 'v', 0, "verbose"},
+    {0, 't', 0, "sort by timestamp"},
     {0, 0, 0, 0},
 };
+
+typedef struct roke_db_entry_s {
+    char* name;
+    char* path;
+    int ndirs;
+    int nfiles;
+    float age;
+} roke_db_entry_t;
+
+static int sort_by_path(const void *a, const void *b)
+{
+    const roke_db_entry_t* ea = (const roke_db_entry_t*) a;
+    const roke_db_entry_t* eb = (const roke_db_entry_t*) b;
+    return strcmp(ea->path, eb->path);
+}
+
+static int sort_by_age(const void *a, const void *b)
+{
+    const roke_db_entry_t* ea = (const roke_db_entry_t*) a;
+    const roke_db_entry_t* eb = (const roke_db_entry_t*) b;
+    float v = ea->age - eb->age;
+    if (v < 0) {
+        return -1;
+    } else if (v > 0) {
+        return 1;
+    }
+    return 0;
+}
 
 /**
  * @brief print basic information about exiting index files
@@ -22,7 +53,8 @@ argparse_spec_t spec[] = {
  */
 int
 roke_list(
-    char* config_dir)
+    char* config_dir,
+    int sort_flag)
 {
 
     char scratch[ROKE_PATH_MAX];
@@ -38,9 +70,12 @@ roke_list(
 
     uint64_t ndirs_total=0, nfiles_total=0;
 
-    fprintf(stdout, "%-24s %9s %9s%7s        %s\n", "Name", "#Dirs", "#Files", "Age", "Root");
+    roke_db_entry_t entries[100];
+    memset(entries, 0, sizeof(entries));
+    int entry_count;
 
-    while ((dir = readdir(d)) != NULL) {
+
+    while ((dir = readdir(d)) != NULL && entry_count < 100) {
 
         // find all databases,
         size_t name_len = strlen(dir->d_name);
@@ -69,20 +104,40 @@ roke_list(
         uint64_t ntime = time(NULL) - mtime;
         float age = ntime / 60.0f / 60.0f;
 
-        ndirs_total += ndirs;
-        nfiles_total += nfiles;
-
-        if (age > 24) {
-            age /= 24.0f;
-            fprintf(stdout, "%-24s %9d %9d %7.2f days  %s\n", name, ndirs, nfiles, age, scratch);
-        } else {
-            fprintf(stdout, "%-24s %9d %9d %7.2f hours %s\n", name, ndirs, nfiles, age, scratch);
-        }
-
+        entries[entry_count].name = strdup(name);
+        entries[entry_count].path = strdup(scratch);
+        entries[entry_count].ndirs = ndirs;
+        entries[entry_count].nfiles = nfiles;
+        entries[entry_count].age = age;
+        entry_count++;
     }
 
+    // sort the entries by path
+    if (sort_flag) {
+        qsort(entries, entry_count, sizeof(roke_db_entry_t), sort_by_age);
+    } else {
+        qsort(entries, entry_count, sizeof(roke_db_entry_t), sort_by_path);
+    }
+
+    fprintf(stdout, "%-24s %9s %9s%7s        %s\n", "Name", "#Dirs", "#Files", "Age", "Root");
+    for (int i=0; i<entry_count; i++) {
+        roke_db_entry_t* entry = &entries[i];
+
+        ndirs_total += entry->ndirs;
+        nfiles_total += entry->nfiles;
+
+        if (entry->age > 24) {
+            fprintf(stdout, "%-24s %9d %9d %7.2f days  %s\n", entry->name, entry->ndirs, entry->nfiles, entry->age/24.0f, entry->path);
+        } else {
+            fprintf(stdout, "%-24s %9d %9d %7.2f hours %s\n", entry->name, entry->ndirs, entry->nfiles, entry->age, entry->path);
+        }
+    }
     fprintf(stdout, "total dirs: %" PRIu64 "\ntotal files: %" PRIu64 "\n", ndirs_total, nfiles_total);
 
+    for (int i=0; i<entry_count; i++) {
+        free(entries[i].name);
+        free(entries[i].path);
+    }
 
     if (d != NULL) {
         closedir(d);
@@ -105,9 +160,14 @@ int main(int argc, char** argv)
         goto exit;
     }
 
+    int sort_flag = 0;
+    if (argparser_get_flag(argparse, 't')) {
+        sort_flag = 1;
+    }
+
     fprintf(stdout, "%s\n", config_dir);
 
-    roke_list(config_dir);
+    roke_list(config_dir, sort_flag);
 
 exit:
     argparser_delete(&argparse);
